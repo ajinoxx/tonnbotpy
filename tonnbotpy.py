5add1bd51e24b1d3e4cfa0a12d124d862f176218
ajinoxx <57328840+ajinoxx@users.noreply.github.com>
2020-09-19 11:35:15 -0400

    Update tonnbotpy.py

--- tonnbotpy.py
+++ tonnbotpy.py
@@ -6,7 +6,7 @@ import random
 myID = 191334024612937729
 
 bellsID = 756194795411603547
-botID = 543620408117690378
+studentID = 543187460021288960
 
 client = commands.Bot(command_prefix = '..')
 
@@ -16,6 +16,7 @@ async def on_ready():
 
 @client.command(aliases=['t'])
 async def test(ctx):
+    print('Test command')
     if ctx.author.id == myID:
         await ctx.send("You're sexy Buechie!")
     else:
@@ -24,7 +25,7 @@ async def test(ctx):
 
 @client.event
 async def on_message(message):
-    if message.channel.id != bellsID and discord.utils.find(lambda r: r.id != botID, message.author.roles):
+    if message.channel.id != bellsID and discord.utils.find(lambda r: r.id == studentID, message.author.roles):
         print("Message is not in bells and it is not made by a bot")
         msgStr = message.content.lower()
         if msgStr.startswith("what") and len(msgStr) <= 6:
